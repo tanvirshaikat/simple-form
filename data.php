775f5b8e0c7715d43e2973d52dc547af2d649df5
Tanvir Shaikat <ta.shaikat@gmail.com>
2018-11-19 22:14:15 +0600

    dbless

--- data.php
+++ data.php
@@ -1,34 +1,4 @@
-<?php 
-
-$db['db_host']= "";
-$db['db_user']= "";
-$db['db_pass']= "";
-$db['db_name']= "";
-
-
-// $db['db_host']= "localhost";
-// $db['db_user']= "root";
-// $db['db_pass']= "";
-// $db['db_name']= "appmarket";
-
-foreach ($db as $key => $value) {
-  define(strtoupper($key), $value);
-}
-
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-$connection->set_charset("utf8");
-$connection->query("SET collation_connection = utf8_general_ci");
-
-
-
-if (!$connection) {
-    die("something wrong in connection" . mysql_errno());
-  }
-
- ?>
-
-
+<?php include "config.php" ?>
 
 <!DOCTYPE>
 <html>
