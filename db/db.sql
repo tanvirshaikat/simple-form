d6ba2be75ba35a047b4b807997ec981e7893f2ec
Tanvir Shaikat <ta.shaikat@gmail.com>
2018-11-19 19:05:21 +0600

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,1 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 19, 2018 at 07:04 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.7
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tanvirsh_form`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_data`
+--
+
+CREATE TABLE `form_data` (
+  `form_id` int(100) NOT NULL,
+  `form_name` varchar(200) NOT NULL,
+  `form_address` varchar(200) NOT NULL,
+  `form_phone` varchar(200) NOT NULL,
+  `form_atl` varchar(200) NOT NULL,
+  `form_card` varchar(200) NOT NULL,
+  `form_ssn` varchar(200) NOT NULL,
+  `form_package` varchar(200) NOT NULL,
+  `form_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `form_data`
+--
+
+INSERT INTO `form_data` (`form_id`, `form_name`, `form_address`, `form_phone`, `form_atl`, `form_card`, `form_ssn`, `form_package`, `form_date`) VALUES
+(8, 'mhcgm', 'bmbnmb', 'nb', 'nmbnm', 'bnmbn', 'bnmbv', 'mbnmbnm', '2018-11-19 12:50:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `form_data`
+--
+ALTER TABLE `form_data`
+  ADD PRIMARY KEY (`form_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `form_data`
+--
+ALTER TABLE `form_data`
+  MODIFY `form_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
