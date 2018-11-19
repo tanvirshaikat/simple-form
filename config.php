16b75e9eac64bf0ed6ab1701a531188a2f372a30
Tanvir Shaikat <ta.shaikat@gmail.com>
2018-11-19 22:13:19 +0600

    Controller

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+$db['db_host']= "";
+$db['db_user']= "";
+$db['db_pass']= "";
+$db['db_name']= "";
+// $db['db_host']= "hostname";
+// $db['db_user']= "username";
+// $db['db_pass']= "pass";
+// $db['db_name']= "dbname";
+foreach ($db as $key => $value) {
+  define(strtoupper($key), $value);
+}
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$connection->set_charset("utf8");
+$connection->query("SET collation_connection = utf8_general_ci");
+if (!$connection) {
+    die("something wrong in connection" . mysql_errno());
+  }
+ ?>
